e8827798d0b0e35bb6848083861ebb74a016b477
Jesú Castillo <jesu.castillo@rankmi.com>
2019-02-20 11:26:50 -0300

    Added child navigation with resolve from parent

--- src/repos/repos.routing.ts
+++ src/repos/repos.routing.ts
@@ -1,4 +1,4 @@
-import {Ng1StateDeclaration, StateProvider, Transition} from "@uirouter/angularjs";
+import {Ng1StateDeclaration, StateProvider, Transition, StateParams} from "@uirouter/angularjs";
 import {IHttpService} from "angular";
 
 export function RoutingConfiguration($stateProvider: StateProvider) {
@@ -25,6 +25,24 @@ export function RoutingConfiguration($stateProvider: StateProvider) {
             component: 'repos',
             onExit: () => {
                 return confirm('You are leaving this state, are you sure?');
+            },
+        },
+        {
+            name: 'index.repos.detail',
+            url: '/:repoId',
+            component: 'user',
+            resolve: {
+                repo: ['$transition$', '$stateParams', ($transition$: Transition, $stateParams: StateParams) => {
+                    return $transition$.injector()
+                        .getAsync('repos')
+                        .then((repos: Array<{ id: number }>) => {
+                            const {repoId} = $stateParams;
+                            return repos.find(({id}) => id === +repoId);
+                        });
+                }],
+            },
+            bindings: {
+                user: 'repo',
             },
         },
     ];
