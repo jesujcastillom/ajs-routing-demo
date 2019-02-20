973f9b452f3f9cadb5ed176155a72e4251bf6f57
Jesú Castillo <jesu.castillo@rankmi.com>
2019-02-19 23:39:52 -0300

    Routing ui-view inside parent

---
+++ src/components/repos/repos.component.ts
@@ -0,0 +1,9 @@
+import {IComponentOptions} from "angular";
+
+const template = require('./repos.component.html');
+export const ReposComponent: IComponentOptions = {
+    template,
+    bindings: {
+        repos: '<',
+    }
+};
